0a13dbd47bb42fc05aee5ea2be80de84e7e993ee
Fredrik de Vibe <fdv@ifi.uio.no>
2017-05-13 01:30:01 +0200

    Make the argument definitions more readable

--- txt2pdf.py
+++ txt2pdf.py
@@ -126,34 +126,83 @@ class PDFCreator(object):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
-parser.add_argument('--font', '-f', default='Courier',
-                    help='Select a font (True Type format) by its full path')
-parser.add_argument('--font-size', '-s', type=float, default=10.0,
-                    help='Size of the font')
-parser.add_argument('--extra-vertical-space', '-v', type=float, default=0.0,
-                    help='Extra vertical space between lines')
-parser.add_argument('--kerning', '-k', type=float, default=0.0,
-                    help='Extra horizontal space between characters')
-parser.add_argument('--media', '-m', default='A4',
-                    help='Select the size of the page (A4, A3, etc.)')
-parser.add_argument('--landscape', '-l', action="store_true", default=False,
-                    help='Select landscape mode')
-parser.add_argument('--margin-left', '-L', type=float, default=2.0,
-                    help='Left margin (in cm unit)')
-parser.add_argument('--margin-right', '-R', type=float, default=2.0,
-                    help='Right margin (in cm unit)')
-parser.add_argument('--margin-top', '-T', type=float, default=2.0,
-                    help='Top margin (in cm unit)')
-parser.add_argument('--margin-bottom', '-B', type=float, default=2.0,
-                    help='Bottom margin (in cm unit)')
-parser.add_argument('--output', '-o', default='output.pdf',
-                    help='Output file')
-parser.add_argument('--author', default='',
-                    help='Author of the PDF document')
-parser.add_argument('--title', default='',
-                    help='Title of the PDF document')
-parser.add_argument('--quiet', '-q', action='store_true', default=False,
-                    help='Title of the PDF document')
+parser.add_argument(
+    '--font',
+    '-f',
+    default='Courier',
+    help='Select a font (True Type format) by its full path')
+parser.add_argument(
+    '--font-size',
+    '-s',
+    type=float,
+    default=10.0,
+    help='Size of the font')
+parser.add_argument(
+    '--extra-vertical-space',
+    '-v',
+    type=float,
+    default=0.0,
+    help='Extra vertical space between lines')
+parser.add_argument(
+    '--kerning',
+    '-k',
+    type=float,
+    default=0.0,
+    help='Extra horizontal space between characters')
+parser.add_argument(
+    '--media',
+    '-m',
+    default='A4',
+    help='Select the size of the page (A4, A3, etc.)')
+parser.add_argument(
+    '--landscape',
+    '-l',
+    action="store_true",
+    default=False,
+    help='Select landscape mode')
+parser.add_argument(
+    '--margin-left',
+    '-L',
+    type=float,
+    default=2.0,
+    help='Left margin (in cm unit)')
+parser.add_argument(
+    '--margin-right',
+    '-R',
+    type=float,
+    default=2.0,
+    help='Right margin (in cm unit)')
+parser.add_argument(
+    '--margin-top',
+    '-T',
+    type=float,
+    default=2.0,
+    help='Top margin (in cm unit)')
+parser.add_argument(
+    '--margin-bottom',
+    '-B',
+    type=float,
+    default=2.0,
+    help='Bottom margin (in cm unit)')
+parser.add_argument(
+    '--output',
+    '-o',
+    default='output.pdf',
+    help='Output file')
+parser.add_argument(
+    '--author',
+    default='',
+    help='Author of the PDF document')
+parser.add_argument(
+    '--title',
+    default='',
+    help='Title of the PDF document')
+parser.add_argument(
+    '--quiet',
+    '-q',
+    action='store_true',
+    default=False,
+    help='Title of the PDF document')
 
 args = parser.parse_args()
 
